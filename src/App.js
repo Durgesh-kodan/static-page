b0d6f6d98af862bf7defbff7be402ebeba3c3e56
Durgesh <durgeshkodan101@gmail.com>
2024-08-11 23:34:18 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Container } from '@mui/material';
-import Banner from './components/Banner';
-import Dashboard from './components/Dashboard';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Container } from "@mui/material";
+import Banner from "./components/Banner";
+import Dashboard from "./components/Dashboard";
+import axios from "axios";
 
 function App() {
-    return ( <
-        >
-        <
-        Dashboard / >
-        <
-        />
-    );
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
 }
 
 export default App;
