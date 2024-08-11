4269416ede53bbee514000fddcc779de2fe88c84
Durgesh <durgeshkodan101@gmail.com>
2024-08-11 22:38:15 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from '@mui/material';
+import Banner from './components/Banner';
+import Dashboard from './components/Dashboard';
+import axios from 'axios';
+
+function App() {
+    return ( <
+        >
+        <
+        Dashboard / >
+        <
+        />
+    );
+}
+
+export default App;
